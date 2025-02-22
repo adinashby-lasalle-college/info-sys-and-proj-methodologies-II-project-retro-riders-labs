d2bffafe57a9eccc0f49335733d4b699a4b8d64e
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2025-02-22 11:23:08 -0500

    Implemented Time Speed Multiplier Structure

--- Wave Riders/Assets/Scripts/PlayerController.cs
+++ Wave Riders/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Transform orientationCam;
 
     [SerializeField] private int moveSpeed = 2;
+    float time;
 
     private void OnEnable()
     {
@@ -27,18 +28,26 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         //Get player position and lock it horizontally to the map
-
-        Debug.Log(InputManager.movementInput.y);
+        CalculateTimePassed();
+        Debug.Log(GetTimeMultiplier());
 
         Vector3 movement;
-        movement = orientationCam.transform.forward * moveSpeed + orientationCam.transform.right * InputManager.movementInput.x * moveSpeed * 5; //* GetTimeMultiplier(); // + move left or right
+        movement = orientationCam.transform.forward * moveSpeed * GetTimeMultiplier() + orientationCam.transform.right * InputManager.movementInput.x * moveSpeed * 5;
         movement.y = rb.velocity.y;
         rb.velocity = movement;
     }
 
     private float GetTimeMultiplier()
     {
-        
-        return 0;
+        float result;
+        result = time / 10;
+         
+
+        return result;
+    }
+    private float CalculateTimePassed()
+    {
+        time = Time.time;
+        return time;
     }
 }
