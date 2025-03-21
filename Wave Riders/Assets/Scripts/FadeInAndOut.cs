3a3742a2843c5529cd354a6f830a62402529a445
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2025-03-20 23:20:11 -0400

    Damage Fade Outline

---
+++ Wave Riders/Assets/Scripts/FadeInAndOut.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInAndOut : MonoBehaviour
+{
+    public static void Fade(GameObject canvas, int TimeToFade)
+    {
+        CanvasGroup tempCanvas = canvas.GetComponent<CanvasGroup>();
+
+        Debug.Log(tempCanvas.gameObject.name);
+
+        do
+        {
+            tempCanvas.alpha += TimeToFade * Time.deltaTime;
+            Debug.Log(tempCanvas.alpha);
+
+        } while (tempCanvas.alpha < 1);
+
+        TimeToFade *= 2;
+
+        do
+        {
+            tempCanvas.alpha -= TimeToFade * Time.deltaTime;
+
+        } while (tempCanvas.alpha > 0);
+    }
+}
