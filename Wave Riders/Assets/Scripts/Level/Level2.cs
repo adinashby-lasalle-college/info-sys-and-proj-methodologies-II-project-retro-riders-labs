5a1ced4033b940397b7c00e4c13f81186ff42d51
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2025-04-05 15:17:38 -0400

    Level2 Added random Cop Placement

--- Wave Riders/Assets/Scripts/Level/Level2.cs
+++ Wave Riders/Assets/Scripts/Level/Level2.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Level2 : Level
 {
+    [SerializeField] private GameObject copLeft;
+    [SerializeField] private GameObject copRight;
+
     // Start is called before the first frame update
     void Awake()
     {
         LevelID = 2;
         GetCoords();
     }
+
+    void Start()
+    {
+        copLeft.SetActive(false);
+        copRight.SetActive(false);
+        PlaceObstacles();
+    }
     public override void PlaceObstacles()
     {
+        System.Random rand = new System.Random();
+        int carInt = rand.Next(0, 2);
 
+        if(carInt == 1)
+        {
+            copLeft.SetActive(true);
+        }
+        else
+        {
+            copRight.SetActive(true);
+        }
     }
 }
