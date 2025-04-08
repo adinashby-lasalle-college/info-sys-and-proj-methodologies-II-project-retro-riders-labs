b67c071572dde69e450befebb506528276980e93
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2025-04-08 10:26:58 -0400

    Level modifications

--- Wave Riders/Assets/Scripts/Level/Level1.cs
+++ Wave Riders/Assets/Scripts/Level/Level1.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Level1 : Level
 {
+    [SerializeField] private List<GameObject> obstacles;
+    [SerializeField] private GameObject SpeedBoost;
+
+    Vector3 obstacleDisplacement = new Vector3(25, 0, 0);
+    Vector3 boostDisplacement = new Vector3(28, 0, 0);
+
     // Start is called before the first frame update
     void Awake()
     {
         LevelID = 1;
         GetCoords();
+        PlaceObstacles();
     }
     public override void PlaceObstacles()
     {
+        System.Random rand = new System.Random();
+        int obsInt = rand.Next(0, 2);
 
+        if (obsInt == 1)
+        {
+            foreach (GameObject obstacle in obstacles)
+            {
+                obstacle.transform.position += obstacleDisplacement;
+            }
+            SpeedBoost.transform.position -= boostDisplacement;
+        }
     }
 }
